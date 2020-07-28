673101631f3b44c3e96a40073d5d4180d6efdb5c
Eddie Parton <eddie.parton@gmail.com>
2020-07-28 12:10:36 +0100

    Added GraphQL APU and Database

--- src/App.js
+++ src/App.js
@@ -1,10 +1,48 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
+import { API } from 'aws-amplify';
+import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
+import { listNotes } from './graphql/queries';
+import { createNote as createNoteMutation, deleteNote as deleteNoteMutation } from './graphql/mutations';
+
+const initialFormState = { name: '', description: '' }
 
 function App() {
+
+  const [notes, setNotes] = useState([]);
+  const [formData, setFormData] = useState(initialFormState);
+
+  useEffect(() => {
+    fetchNotes();
+  }, []);
+
+  async function fetchNotes() {
+    //uses the API class to send a query to the GraphQL API and retrieve a list of notes.
+    const apiData = await API.graphql({ query: listNotes });
+    setNotes(apiData.data.listNotes.items);
+  }
+
+  async function createNote() {
+    /*
+    also uses the API class to send a mutation to the GraphQL API,
+    the main difference is that in this function we are passing in the variables needed for a GraphQL mutation so that we can create a new note with the form data.
+    */
+    if (!formData.name || !formData.description) return;
+    await API.graphql({ query: createNoteMutation, variables: { input: formData } });
+    setNotes([ ...notes, formData ]);
+    setFormData(initialFormState);
+  }
+
+  async function deleteNote({ id }) {
+    // Like createNote, this function is sending a GraphQL mutation along with some variables, but instead of creating a note we are deleting a note.
+    const newNotesArray = notes.filter(note => note.id !== id);
+    setNotes(newNotesArray);
+    await API.graphql({ query: deleteNoteMutation, variables: { input: { id } }});
+  }
+
   return (
+    /*
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -23,6 +61,35 @@ function App() {
       </header>
       <AmplifySignOut />
     </div>
+    */
+
+   <div className="App">
+   <h1>My Notes App</h1>
+   <input
+     onChange={e => setFormData({ ...formData, 'name': e.target.value})}
+     placeholder="Note name"
+     value={formData.name}
+   />
+   <input
+     onChange={e => setFormData({ ...formData, 'description': e.target.value})}
+     placeholder="Note description"
+     value={formData.description}
+   />
+   <button onClick={createNote}>Create Note</button>
+   <div style={{marginBottom: 30}}>
+     {
+       notes.map(note => (
+         <div key={note.id || note.name}>
+           <h2>{note.name}</h2>
+           <p>{note.description}</p>
+           <button onClick={() => deleteNote(note)}>Delete note</button>
+         </div>
+       ))
+     }
+   </div>
+   <AmplifySignOut />
+ </div>
+
   );
 }
 
--- src/aws-exports.js
+++ src/aws-exports.js
@@ -7,7 +7,11 @@ const awsmobile = {
     "aws_cognito_region": "us-east-1",
     "aws_user_pools_id": "us-east-1_wTQPgMnkc",
     "aws_user_pools_web_client_id": "fipclbnvg3pcqf9l8c8pl5558",
-    "oauth": {}
+    "oauth": {},
+    "aws_appsync_graphqlEndpoint": "https://jbi32dwbrvcibnowcn7tzl6eiu.appsync-api.us-east-1.amazonaws.com/graphql",
+    "aws_appsync_region": "us-east-1",
+    "aws_appsync_authenticationType": "API_KEY",
+    "aws_appsync_apiKey": "da2-rtctl6xel5calch2al7torxgom"
 };
 
 
